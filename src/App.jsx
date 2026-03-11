60868ff7f8e03f9da76dd746ec7afa3707185f0c
luuun0617 <ashin950@gmail.com>
2026-03-11 22:07:46 +0800

    0311 更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,172 @@
 import { useState } from 'react'
-import axios from 'axios';
-
+import axios from "axios";
 import "./assets/style.css";
 
 const API_BASE = import.meta.env.VITE_API_BASE;
 const API_PATH = import.meta.env.VITE_API_PATH;
 
-
-
-
-
 function App() {
-  const [data,setData] = useState({
-    username :'',
-    password :'',
-  })
-  
-  async function login(){
-    console.log('login');
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [isAuth, setIsAuth] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [tempProduct, setTempProduct] = useState(null);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((preData) => ({
+      ...preData,
+      [name]: value,
+    }))
   }
 
-  function eventHandler(e){
-    const {value,name} =e.target;
-    console.log(value,name);
-     setData({
-     ...data,
-     [name] : value
-   })
+  const getProducts = async () => {
+    try {
+      const response = await axios.get(`${API_BASE}/api/${API_PATH}/admin/products`);
+      setProducts(response.data.products);
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
+
+  const onSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const response = await axios.post(`${API_BASE}/admin/signin`, formData);
+      const {token, expired} = response.data;
+
+      document.cookie = `hexToken=${token};expires=${new Date(expired)}`;
+      axios.defaults.headers.common['Authorization'] = token;
+      
+      getProducts();
+      setIsAuth(true);
+
+    } catch (error) {
+      console.log(error.response?.data);
+
+      setIsAuth(false);
+    }
+  }
+
+  const checkLogin = async () => {
+    try {
+      const token = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("hexToken="))
+      ?.split("=")[1];
+
+      axios.defaults.headers.common['Authorization'] = token;
+
+      const response = await axios.post(`${API_BASE}/api/user/check`);
+      console.log(response.data);
+
+    } catch (error) {
+      console.log(error.response?.data.message)
+    }
   }
 
   return (
     <>
-      <div className="container login">
-        <div className="row justify-content-center">
-          <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
-          <div className="col-8">
-            <form id="form" className="form-signin">
-              <div className="form-floating mb-3">
-                <input type="email" className="form-control" 
-                  name='username' placeholder="name@example.com" onChange={(e)=>{ eventHandler(e)
-                }} />
-                <label htmlFor="username">Email address</label>
-              </div>
-              <div className="form-floating">
-                <input type="password" className="form-control" 
-                  name='password' placeholder="Password"  onChange={(e)=>{ eventHandler(e)
-                  }}  />
-                <label htmlFor="password">Password</label>
-              </div>
-              <button type='submit' id='login' className='btn btn-primary w-100 mt-3' onClick={()=>login()}>登入</button>
-
-            </form>
-
-          </div>
+    {
+      !isAuth ? (      
+        <div className="container login">
+          <h1>請先登入</h1>
+          <form className="form-floating" onSubmit={(e)=> onSubmit(e)}>
+            <div className="form-floating mb-3">
+              <input type="email" className="form-control" name="username" placeholder="name@example.com" value={formData.username} onChange={(e) => handleInputChange(e)}/>
+              <label htmlFor="username">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input type="password" className="form-control" name="password" placeholder="Password" value={formData.password} onChange={(e) => handleInputChange(e)}/>
+              <label htmlFor="password">Password</label>
+            </div> 
+            <button type="submit" className="btn btn-primary w-100 mt-2">登入</button>    
+          </form>
         </div>
-        
-        
-      </div>
+      ) : (
+        <div className="container">
+          <button
+            className="btn btn-danger mb-5"
+            type="button"
+            onClick={() => checkLogin()}
+          >
+            確認是否登入
+          </button>  
 
+          <div className="container">
+            <div className="row mt-5">
+              <div className="col-md-6">
+                <h2>產品列表</h2>
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>產品名稱</th>
+                      <th>原價</th>
+                      <th>售價</th>
+                      <th>是否啟用</th>
+                      <th>查看細節</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {products.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.title}</td>
+                        <td>{item.origin_price}</td>
+                        <td>{item.price}</td>
+                        <td>
+                          {item.is_enabled ? "啟用" : "未啟用"}
+                        </td>
+                        <td>
+                          <button className="btn btn-primary" onClick={() => {
+                              return setTempProduct(item);
+                            }}>查看細節</button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <div className="col-md-6">
+                <h2>單一產品細節</h2>
+                {tempProduct ? (
+                  <div className="card mb-3">
+                    <img src={tempProduct.imageUrl} className="card-img-top primary-image" alt="主圖" />
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        {tempProduct.title}
+                        <span className="badge bg-primary ms-2">{tempProduct.category}</span>
+                      </h5>
+                      <p className="card-text">商品描述：{tempProduct.description}</p>
+                      <p className="card-text">商品內容：{tempProduct.content}</p>
+                      <div className="d-flex">
+                        <p className="card-text text-secondary"><del>{tempProduct.origin_price}</del></p>
+                        元 / {tempProduct.price} 元
+                      </div>
+                      <h5 className="mt-3">更多圖片：</h5>
+                      <div className="d-flex flex-wrap">
+                        {
+                          tempProduct.imagesUrl.map((item, index) => {
+                            return <img src={item} alt="展示圖片" className="images" key={index} />;
+                          })
+                        }
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <p className="text-secondary">請選擇一個商品查看</p>
+                )}
+              </div>
+            </div>
+          </div>          
+
+        </div>
+      )
+    }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/week2-0301/',
   plugins: [react()],
 })
